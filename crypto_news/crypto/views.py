d86be556fe0b43c9ce4c1a9ac624d9be56f93549
e-landeros <landerosedgard@gmail.com>
2019-02-06 16:42:48 -0800

    extending template update markup use api

--- crypto_news/crypto/views.py
+++ crypto_news/crypto/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+import requests
+import json
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    api_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
+    api = json.loads(api_request.content)
+
+    context = {
+        'api':api
+    }
+
+    return render(request, 'home.html', context)
